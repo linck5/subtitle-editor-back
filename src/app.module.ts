9725a0a4737462e042046a1a2239d6c4b3d74027
Larss <larslarsandersen@gmail.com>
2018-06-14 16:46:21 -0300

    Removed message sender test

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,12 @@ import { CommonModule } from './common/common.module';
 //import { DBConfigModule } from './dbConfig/dbConfig.module';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
-import { MessageSenderModule } from './messageSender/messageSender.module';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
     imports: [
       MongooseModule.forRoot(process.env.DB_CONN),
-      AuthModule, UserModule, CommonModule, MessageSenderModule
+      AuthModule, UserModule, CommonModule
     ],
 
 })
--- src/messageSender/messageSender.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { MessageSenderService } from './messageSender.service';
-import { MessageSenderController } from './messageSender.controller';
-@Module({
-    providers: [MessageSenderService],
-    controllers: [MessageSenderController]
-})
-export class MessageSenderModule {
-}
