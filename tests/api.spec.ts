78d411a2b0e641fe207dcc527c71f87ef935e9ef
Larss <larslarsandersen@gmail.com>
2018-08-16 17:32:22 -0300

    Api tests

--- tests/api.spec.ts
+++ tests/api.spec.ts
@@ -1,8 +1,7 @@
 import * as request from 'supertest';
 import { getNestMongoApp } from './setupServer';
+var fs = require('fs');
 require('jest');
-
-import { test } from './subtitle.subtest'
 
 
 
@@ -10,24 +9,124 @@ import { test } from './subtitle.subtest'
 jest.setTimeout(1000 * 60); //one minute
 
 
-describe('Subtitles', () => {
+describe('Api Tests', () => {
 
 
-    let server;
+  var server;
+  var testData = {
+    users: {
+      adm1: null,
+      user1: null,
+    },
+    videos: {
+      video1: null,
+      video2: null
+    },
+    subtitles: {
+      subtitle1: null
+    }
+  };
 
-    beforeAll(async () => {
-      server = await getNestMongoApp();
+  beforeAll(async () => {
+    server = await getNestMongoApp();
+  });
 
-      test(server);
+  describe('Insert some test data on mock db', () => {
+
+    it('should /POST some users', async () => {
+      let res1 = await request(server)
+        .post('/users')
+        .send({
+          username: "adm1",
+          password: "adm1pw",
+          roles: ["MODERATOR", "ADMIN"]
+        })
+        .expect(201);
+
+      let res2 = await request(server)
+        .post('/users')
+        .send({
+          username: "user1",
+          password: "user1pw"
+        })
+        .expect(201);
+
+      testData.users.adm1 = res1.body;
+      testData.users.user1 = res2.body;
+
+
+    });
+
+    it('should /POST some videos', async () => {
+      let res1 = await request(server)
+        .post('/videos')
+        .send({
+          name:"灰と幻想のグリムガル Episode 9",
+          description:"Hai to Gensou no Grimgar - Fear, survival, instinct. Thrown into a foreign land with nothing but hazy memories and the knowledge of their name, they can feel only these three emotions resonating deep within their souls. A group of strangers is given no other choice than to accept the only paying job in this game-like world—the role of a soldier in the Reserve Army—and eliminate anything that threatens the peace in their new world, Grimgar. When all of the stronger candidates join together, those left behind must create a party together to survive: Manato, a charismatic leader and priest; Haruhiro, a nervous thief; Yume, a cheerful hunter; Shihoru, a shy mage; Mogzo, a kind warrior; and Ranta, a rowdy dark knight. Despite its resemblance to one, this is no game—there are no redos or respawns; it is kill or be killed. It is now up to this ragtag group of unlikely fighters to survive together in a world where life and death are separated only by a fine line.",
+          duration: 1000 * 60 * 23,
+          url: "https://animelon.com/video/5aac0620b6b8f36034bd33bb"
+        })
+        .expect(201);
+
+      let res2 = await request(server)
+        .post('/videos')
+        .send({
+          name:"Cowboy Bebop Episode 6",
+          description:"カウボーイビバップ - 全般にわたって漂うクールなカッコよさ、ジャズのリズムで贈るスタイリッシュなハードボイルドSFアクション。ワープゲートで各惑星が結ばれた2071年の太陽系を舞台に、元マフィアと元警官という経歴の賞金稼ぎスパイクとジェット、記憶喪失の上に莫大な借金を背負っている謎の女フェイ、見た目は無垢な少年、実は少女の天才ハッカーエド、人間並みの知能を持つデータ犬アインという4人と1匹が、運命のいたずらにより奇妙な共同生活を送ることになる。共に暮らしながらも微妙な距離感を保つ4人のセッションは、ときには激しく、ときには滑稽で、ときには切ないエピソードを奏でる。 また、遊び心とスパイスの効いた楽曲の数々は、カウボーイビバップならではのスタイリッシュな雰囲気作りに一役も二役も買っている。先の見えない賞金稼ぎを続けながら、彼らはなにを追い求めるのか?広大な宇宙を舞台に、賞金稼ぎが繰り広げるスペースジャズドラマ!",
+          duration: 1481000,
+          url: "https://animelon.com/video/5ac58b78f1fe4e742d177fc7"
+        })
+        .expect(201);
+
+      testData.videos.video1 = res1;
+      testData.videos.video2 = res2;
+    });
+
+
+    it(`should /POST a simple sub`, async () => {
+
+        let res = await request(server)
+          .post('/subtitles')
+          .send({
+              lines: [
+                {
+                  text: "test111",
+                  startTime: 9400,
+                  endTime: 10100
+                },
+                {
+                  text: "test漢字ひらがなカタカナ",
+                  startTime: 999888777666,
+                  endTime: 0
+                }
+              ]
+          })
+          .expect(201);
+
+        expect(res.body.lines.length).toBe(2);
+        expect(res.body.lines[0].id).toBe(0);
+        expect(res.body.lastId).toBe(1);
+
+    });
+
+
+    it(`should /POST a sub from an ass string`, async () => {
+
+        let res = await request(server)
+          .post('/subtitles/fromAss')
+          .send({
+              assstring: fs.readFileSync('./tests/kaiji12.ass', "utf8")
+          })
+          .expect(201);
+
+        expect(res.body.lastId).toBe(472);
+
+        testData.subtitles.subtitle1 = res;
     });
 
 
 
-    it(`/GETs users`, async(done) => {
-        const subtitle = await request(server)
-            .get('/users' )
-            .expect(200);
+  });
 
-        done();
-    });
+
 });
