bae14da2f4ec7e106f9581999c9a977d2d2e1ec1
Larss <larslarsandersen@gmail.com>
2018-08-26 13:49:02 -0300

    Added fields to subtitle schema

--- src/subtitle/subtitle.schema.ts
+++ src/subtitle/subtitle.schema.ts
@@ -1,17 +1,27 @@
 import { Document, Schema } from 'mongoose';
 import { Line, LineSchema } from './line/line.schema';
 
+
+
 export interface Subtitle extends Document {
+  format: string;
+  original: string;
   lines: Line[];
   lastId: number;
 }
 
 export const SubtitleSchema = new Schema({
+
+  format: {
+    type: String
+  },
+  original: {
+    type: String
+  },
   lines: {
     type: [LineSchema],
     required: true
   },
-
   lastId: {
     type: Number,
     required: true
